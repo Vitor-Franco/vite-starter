02e2dba5419bec3d2933fc277a3e090c4483270f
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-18 17:18:59 -0300

    add meta.env

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import image from './steve-after-a-workshop.jpg';
 import favicon from './favicon-16x16.png';
 import imageTooled from './steve-after-a-workshop.jpg?h=400&format=webp';
 
+console.log(import.meta.env);
+
 import('./counter').then(({ initializeCounter }) => {
   initializeCounter();
 });
