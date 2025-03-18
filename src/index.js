58416eca1f3ff15016ede93a354edeccd1b93109
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-18 14:28:17 -0300

    test static assets dynamic importing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // import { initializeCounter } from './counter';
+import image from './steve-after-a-workshop.jpg';
+console.log('🚀 ~ file: index.js:3 ~ image:', image);
+import favicon from './favicon-16x16.png';
+console.log('🚀 ~ file: index.js:4 ~ favicon:', favicon);
+// console.log("🚀 ~ file: index.js:3 ~ image:", image)
 
 import('./counter').then(({ initializeCounter }) => {
   initializeCounter();
