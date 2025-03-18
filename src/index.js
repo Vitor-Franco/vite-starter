04ee52b436b5f5e9a08c3c6b5b94d0ac33fc4be6
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-18 17:18:48 -0300

    add image tool

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 // import { initializeCounter } from './counter';
 import image from './steve-after-a-workshop.jpg';
-console.log('🚀 ~ file: index.js:3 ~ image:', image);
 import favicon from './favicon-16x16.png';
-console.log('🚀 ~ file: index.js:4 ~ favicon:', favicon);
-// console.log("🚀 ~ file: index.js:3 ~ image:", image)
+import imageTooled from './steve-after-a-workshop.jpg?h=400&format=webp';
 
 import('./counter').then(({ initializeCounter }) => {
   initializeCounter();
