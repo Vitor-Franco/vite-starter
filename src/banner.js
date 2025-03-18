9f0d4ceaad7bfab2eb7ec5825aba3e306e77391f
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-18 12:19:11 -0300

    add dynamic css import

--- src/banner.js
+++ src/banner.js
@@ -1,3 +1,7 @@
+import { banner } from './banner.module.css';
+
 export function showBanner(text) {
-  document.querySelector('#content').textContent = text;
+  const content = document.querySelector('#content');
+  content.textContent = text;
+  content.classList.add(banner);
 }
