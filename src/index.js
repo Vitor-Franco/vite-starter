03f89476ea8306182812f09a9bd52d400386f35f
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-18 21:28:49 -0300

    add vite md plugin

--- src/index.js
+++ src/index.js
@@ -1,19 +1,2 @@
-// const svgModules = import.meta.glob('./logos/**/*.svg');
-// const svgImages = Object.values(svgModules).map(async (module) => module());
-
-// for await (const image of svgImages) {
-//   const img = document.createElement('img');
-//   img.src = image.default;
-//   img.height = 120;
-//   document.body.appendChild(img);
-// }
-
-const images = Object.values(
-  import.meta.glob('./logos/**/*.svg', { eager: true }),
-);
-for (const image of images) {
-  const img = document.createElement('img');
-  img.src = image.default;
-  img.height = 120;
-  document.body.appendChild(img);
-}
+import paul from './biographies/paul.md';
+console.log(paul);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
+import MarkdownIt from 'markdown-it';
+import { readFile } from 'node:fs/promises';
+
+const md = new MarkdownIt();
+
+const renderMarkdown = async (file) => {
+  const content = await readFile(file, 'utf-8');
+  return md.render(content);
+};
+
+function markdownToHtml() {
+  return {
+    name: 'markdown-to-html',
+    resolveId(id) {
+      if (id.endsWith('.md')) {
+        return id;
+      }
+
+      return null;
+    },
+    async load(id) {
+      if (id.endsWith('.md')) {
+        const rendered = await renderMarkdown(id);
+        return `export default ${JSON.stringify(rendered)}`;
+      }
+      return null;
+    },
+  };
+}
+
 export default defineConfig({
-  plugins: [imagetools()],
+  plugins: [imagetools(), markdownToHtml()],
 });
